fc392b0a6282480583edde634ccbcc2dd5dfcbd2
shehan1215 <kaushikashehan123@gmail.com>
2024-04-04 13:53:42 +0530

    Connect to the database of MySql and created the user validation if user name and password also correct

--- Login/Login/Form1.cs
+++ Login/Login/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 namespace Login
 {
@@ -20,24 +21,52 @@ namespace Login
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "")
-            {
-                MessageBox.Show("Enter the username");
-            }
-            else if (textBox2.Text == "")
-            {
-                MessageBox.Show("Enter the Password");
-            }
-            else 
-            {
-                try
-                {
+            Login();  
+        }
+        private void Login()
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
 
-                }
-                catch (Exception ex)
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter both username and password");
+                return;
+            }
+
+            string MySQLConnectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=test";
+            MySqlConnection databaseConnection = new MySqlConnection(MySQLConnectionString);
+
+            try
+            {
+                databaseConnection.Open();
+
+                // Query to check if username and password match
+                string query = "SELECT * FROM user WHERE userName = @username AND Password = @password";
+                MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+                commandDatabase.Parameters.AddWithValue("@username", username);
+                commandDatabase.Parameters.AddWithValue("@password", password);
+
+                MySqlDataReader reader = commandDatabase.ExecuteReader();
+
+                if (reader.Read())
                 {
-                    MessageBox.Show("" + ex);
+                    MessageBox.Show("Login Successful");
                 }
+                else
+                {
+                    MessageBox.Show("Invalid username or password");
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                databaseConnection.Close();
             }
         }
     }
